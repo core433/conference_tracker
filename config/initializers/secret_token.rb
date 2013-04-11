8f75007ea87358d830c705f816e54ab321cf6576
Daniel Chang <core433@gmail.com>
2013-04-10 21:47:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConferenceTracker::Application.config.secret_token = '92dbc9f19b8a399e7885934e1f9b3bfddf53b8c57bd7f0512a53d905fc52a0f90de4244e80f4fffb5775fb59f5edeaaf36dc373551fa202c73d4aff4e9d111f5'
