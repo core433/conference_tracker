6b778d4c12e4ccc4c034c385ee0416c4ec350017
Daniel Chang <core433@gmail.com>
2013-04-12 20:56:45 -0700

    added sessions sign in and sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  # Froce signout to prevent CSRF attacks
+  def handle_unferified_request
+  	sign_out
+  	super
+  end
 end
