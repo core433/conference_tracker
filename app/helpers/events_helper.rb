9f30d7c15610634c5c1bc408597fd9eb45fbcd24
Daniel Chang <core433@gmail.com>
2013-04-14 00:13:20 -0700

    parsing in a good place, showing up to 10 days out, events displayed by date

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,19 @@
 module EventsHelper
+
+	# Currently returns a list of DateTime objects, starting
+	# from today, to 10 days out in the future.  In the future,
+	# will need to extend this so that it can take a start date
+	# and an end date, and provide those dates
+	def getDatesToDisplay()
+
+		numDays = 10
+		retDays = []
+
+		for i in 0..10
+			a_date = (DateTime.now + i.days)
+			retDays.push(a_date)
+		end
+
+		return retDays
+	end
 end
